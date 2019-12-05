364e367a5f5ce06577a1cd42ff82540fa5dbe28f
unknown <shymechko.m.a@gmail.com>
2019-12-05 15:24:05 +0200

    commit

---
+++ src/scripts/addEvent.js
@@ -0,0 +1,36 @@
+const btnSend = document.querySelector('.submit-button');
+const btnClose = document.querySelector('.close');
+
+
+const addEvent = (event) => {
+    event.preventDefault();
+    let inputName = document.querySelector('.input__name');
+    let inputStartDate = document.querySelector('.start-date');
+    let inputStartTime = document.querySelector('.start-time');
+    let inputEndDate = document.querySelector('.end-date');
+    let inputEndTime = document.querySelector('.end-time');
+    let inputDescription = document.querySelector('.description-input');
+
+    events.push({
+        id: events.length + 1,
+        name: inputName.value,
+        createDate: new Date(),
+        startDateEvent: inputStartDate.value,
+        stratTimeEvent: inputStartTime.value,
+        endDateEvent: inputEndDate.value,
+        endTimeEvent: inputEndTime.value,
+        description: inputDescription.value
+    })
+
+
+    console.log(events)
+}
+
+const close = (event) => {
+    event.preventDefault();
+    const popup = document.querySelector(`.popup`);
+    popup.classList.remove('popup-on')
+}
+
+btnSend.addEventListener('click', addEvent)
+btnClose.addEventListener('click', close)
---
+++ src/scripts/sorage.js
@@ -0,0 +1,10 @@
+let events = [{
+    id: 1,
+    name: 'event',
+    createDate: '12-12-2019',
+    startDateEvent: '12-12-2019',
+    stratTimeEvent: '12:00',
+    endDateEvent: '12-12-2019',
+    endTimeEvent: '13:00',
+    description: ''
+}];
