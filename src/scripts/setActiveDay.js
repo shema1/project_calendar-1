fcb3c2367081dfe98ccf08696cdffefddb52cf00
eposha <freedoctime@gmail.com>
2019-12-05 17:53:33 +0300

    today-button and set current month

--- src/scripts/setActiveDay.js
+++ src/scripts/setActiveDay.js
@@ -1,5 +1,5 @@
-const weekDaysElems = document.querySelectorAll('.days__numbe');
-const currentNumberDay = new Date().getDay() - 1;
+// const weekDaysElems = document.querySelectorAll('.days__numbe');
+// const currentNumberDay = new Date().getDay() - 1;
 
 
-[...weekDaysElems].find(arg => arg.dataset.blockNumber == currentNumberDay).classList.add('active-day-number');
+// [...weekDaysElems].find(arg => arg.dataset.blockNumber == currentNumberDay).classList.add('active-day-number');
