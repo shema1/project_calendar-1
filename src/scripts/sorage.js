683c8d1a99ee54914c7ff3ecefa23a48077324b0
unknown <shymechko.m.a@gmail.com>
2019-12-06 10:52:57 +0200

    commit

--- src/scripts/sorage.js
+++ src/scripts/sorage.js
@@ -3,8 +3,6 @@ let events = [{
     name: 'event',
     createDate: '12-12-2019',
     startDateEvent: '12-12-2019',
-    stratTimeEvent: '12:00',
     endDateEvent: '12-12-2019',
-    endTimeEvent: '13:00',
     description: ''
 }];
