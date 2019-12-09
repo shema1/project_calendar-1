45b29ff5bccee641762e7b037907102442a1364e
eposha <freedoctime@gmail.com>
2019-12-09 20:42:52 +0300

    fix-render-events

--- src/scripts/renderEvent.js
+++ src/scripts/renderEvent.js
@@ -1,23 +1,30 @@
 let now;
 let end;
 let selector;
+let getHours;
 let test;
 let parent;
+let testRend = [];
 
 
 const renderEvents = () => {
 
     events.map(elem => {
         now = new Date(`${elem.startDateEvent}`);
-        end = new Date(`${elem.endDateEvent}`)
-        selector = `${now.getFullYear()+'-'}${now.getMonth()+1+'-'}${check(now.getDate())}`
-        parent = document.querySelector(`[id='${now.getHours()}']`);
-        if (parent === null) return
+        end = new Date(`${elem.endDateEvent}`);
+        selector = `${now.getFullYear()+'-'}${now.getMonth()+1+'-'}${check(now.getDate())}`;
+        getHours = now.getHours();
+        if (getHours < 10) {
+            getHours = `0${now.getHours()}`;
+        };
+        parent = document.querySelector(`[id='${getHours}']`);
+        // console.log(now.getHours());
+        if (parent === null) return;
         test = parent.querySelector(`[id='${selector}']`);
         if (test === null) {
             console.log('The event cannot be displayed at a specified interval')
-            return
-        }
+            return;
+        };
 
         let hours = now.getHours();
         let minutes = now.getMinutes();
@@ -41,8 +48,10 @@ const renderEvents = () => {
         ${elem.description}
         </span>
         </div>`
+        testRend.push(aaa);
         test.innerHTML = aaa;
     })
+    console.log(new Date('2019-12-12T02:00').getHours());
 }
 
 renderEvents();
