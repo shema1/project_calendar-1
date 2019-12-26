4a837eae944b6ba5648fb6e340d18afd3155d73b
eposha <freedoctime@gmail.com>
2019-12-26 21:42:40 +0300

    jest

---
+++ src/scripts/index.test.js
@@ -0,0 +1,15 @@
+// import {
+//     createEvents,
+//     getEventsList,
+//     updateEvents,
+//     getEvents,
+//     deleteEvents
+// } from './eventsGateaway.js';
+
+// it('length of array after DELETE should be equal length before DELETE', () => {
+
+//     deleteEvents(0);
+
+
+//     expect(result).toEqual(null);
+// })
